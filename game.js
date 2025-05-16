e0c6da913d52e9b67fa2cc87490f2fea07e7f830
TheKrakken11 <clarkwwhalen@gmail.com>
2025-05-16 13:50:48 -0400

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ function init3D() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 	renderer = new THREE.WebGLRenderer();
+	renderer.domElement.id = 'gameCanvas';
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 	const geometry = new THREE.BoxGeometry();
