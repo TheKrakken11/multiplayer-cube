f4bba9ce7b75ce855da757a8f8d6097b813639c8
TheKrakken11 <clarkwwhalen@gmail.com>
2025-05-21 17:53:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import * as Peer from '
+import * as Peer from 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js';
 import {
   computeBoundsTree,
   disposeBoundsTree,
